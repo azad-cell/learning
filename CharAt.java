57eb769721fa670f2a0f2064f953fcde7062d5d5
azad-cell <53278761+azad-cell@users.noreply.github.com>
2019-07-24 17:27:31 -0400

    Create CharAt.java

---
+++ CharAt.java
@@ -0,0 +1,13 @@
+package myfamily;
+
+public class CharAt {
+	public static void main(String[] args) {
+		String president = "Washington";
+		char myChar = president.charAt(5);
+		System.out.println(myChar);
+		char myChar1 = president.charAt(3);
+		System.out.println(myChar1);
+
+	}
+
+}
